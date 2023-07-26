6ac770c7bf86f92fd94d6bdff499cd413aaf8ce6
Filonov <filonovia@gmail.com>
2023-07-26 23:18:40 +0500

    part8 profilng

---
+++ part8/pkg/index.go
@@ -0,0 +1,27 @@
+package index
+
+import (
+	"Goondex/part8/pkg/crawler"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
+var Words = make(map[string][]int)
+
+func IndexDocs(docs []crawler.Document) {
+	for _, doc := range docs {
+		words := strings.Split(strings.ToLower(doc.Title), " ")
+		for _, word := range words {
+			indexes := Words[word]
+			if indexes == nil {
+				Words[word] = []int{doc.ID}
+			} else {
+				if slices.Contains(indexes, doc.ID) {
+					continue
+				}
+				Words[word] = append(indexes, doc.ID)
+			}
+		}
+	}
+}
