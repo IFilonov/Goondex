82344ddfac683a76f50536c30fa744e0fb2ef1fb
Filonov <filonovia@gmail.com>
2023-07-26 10:39:43 +0500

    part6 simplyfizing code

--- part6/pkg/hw.go
+++ part6/pkg/hw.go
@@ -1,25 +1,18 @@
 package hw
 
 import (
-	"log"
+	"errors"
 	"math"
 )
 
-// По условиям задачи, координаты не могут быть меньше 0.
-type Geom struct {
-	X1, Y1, X2, Y2 float64
-}
+// Убрана структура Geom
+// Функция реализована без структуры
+// Поправлен тест без структуры
 
-// добавлен конструктор
-func New(x1, y1, x2, y2 float64) *Geom {
-	if x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0 {
-		log.Fatal("Координаты не могут быть меньше нуля")
+func CalculateDistance(X1, Y1, X2, Y2 float64) (float64, error) {
+	if X1 < 0 || X2 < 0 || Y1 < 0 || Y2 < 0 {
+		return -1, errors.New("Координаты не могут быть меньше нуля")
 	}
-	geom := Geom{X1: x1, Y1: y1, X2: x2, Y2: y2}
-	return &geom
-}
 
-// добавлена функция типа Geom
-func (g *Geom) CalcDist() (distance float64) {
-	return math.Sqrt(math.Pow(g.X2-g.X1, 2) + math.Pow(g.Y2-g.Y1, 2))
+	return math.Sqrt(math.Pow(X2-X1, 2) + math.Pow(Y2-Y1, 2)), nil
 }
--- part6/pkg/hw_test.go
+++ part6/pkg/hw_test.go
@@ -2,22 +2,28 @@ package hw
 
 import "testing"
 
-func TestGeom_CalculateDistance(t *testing.T) {
+func Test_CalculateDistance(t *testing.T) {
 	tests := []struct {
 		name         string
-		geom         *Geom
+		X1           float64
+		X2           float64
+		Y1           float64
+		Y2           float64
 		wantDistance float64
 	}{
 		{
 			name:         "#1",
-			geom:         New(1, 1, 4, 5),
+			X1:           1,
+			X2:           4,
+			Y1:           1,
+			Y2:           5,
 			wantDistance: 5,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotDistance := tt.geom.CalcDist(); gotDistance != tt.wantDistance {
-				t.Errorf("Geom.CalcDist() = %v, want %v", gotDistance, tt.wantDistance)
+			if gotDistance, err := CalculateDistance(tt.X1, tt.Y1, tt.X2, tt.Y2); gotDistance != tt.wantDistance && err == nil {
+				t.Errorf("Geom.CalculateDistance() = %v, want %v", gotDistance, tt.wantDistance)
 			}
 		})
 	}
