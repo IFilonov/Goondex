c4915501931f516ff19861ac5218719a5eade86b
Filonov <filonovia@gmail.com>
2023-07-30 09:25:56 +0500

    part11 save scan urls before port openning

--- part11/pkg/netsrv/netsrv.go
+++ part11/pkg/netsrv/netsrv.go
@@ -2,17 +2,28 @@
 package netsrv
 
 import (
+	"Goondex/part11/pkg/crawler"
 	"Goondex/part11/pkg/crawler/spider"
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
 const addr = "0.0.0.0:8000"
 const depth = 2
 
+var urls = []string{"https://go.dev", "https://github.com/"}
+
 func StartNetSrv() error {
+	var allDocs []crawler.Document
+	allDocs, err := scanDocs()
+	if err != nil {
+		fmt.Println("Ошибка сканирования")
+		return err
+	}
+
 	listener, err := net.Listen("tcp4", addr)
 	if err != nil {
 		fmt.Printf("Ошибка открытия порта %v: %v\n", addr, err)
@@ -20,47 +31,61 @@ func StartNetSrv() error {
 	}
 
 	for {
-		// Принимаем подключение.
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Printf("Ошибка подключения: %v\n", err)
 			continue
 		}
-		// Вызов обработчика подключения.
-		go handleConn(conn)
+		go handleConn(conn, allDocs)
 	}
 }
 
-func handleConn(conn net.Conn) {
+func handleConn(conn net.Conn, allDocs []crawler.Document) {
 	defer conn.Close()
 	conn.SetDeadline(time.Now().Add(time.Second * 600))
 	r := bufio.NewReader(conn)
 	for {
-		url, _, err := r.ReadLine()
+		word, _, err := r.ReadLine()
 		if err != nil {
 			fmt.Printf("Ошибка чтения из соединения: %v\n", err)
 			return
 		}
-		s := spider.New()
-		u := string(url)
-		fmt.Println("Пришло сообщение: ", u)
-		docs, err := s.Scan(u, depth)
+
+		fmt.Println("Пришло сообщение: ", string(word))
+
+		err = sendUrls(allDocs, string(word), conn)
 		if err != nil {
-			fmt.Printf("Ошибка сканирования %v: %v\n", u, err)
+			fmt.Printf("Ошибка записи : %v\n", err)
 			return
 		}
 
-		for i := 0; i < len(docs); i++ {
-			msg := docs[i].URL + " " + docs[i].Title + "\n"
-			_, err = conn.Write([]byte(msg))
-			if err != nil {
-				fmt.Printf("Ошибка записи : %v\n", err)
-				return
-			}
-			fmt.Println("Передача обратно: ", msg)
-		}
-		_, err = conn.Write([]byte("--the end--\n"))
-
 		conn.SetDeadline(time.Now().Add(time.Second * 600))
 	}
+}
+
+func scanDocs() ([]crawler.Document, error) {
+	var allDocs []crawler.Document
+	s := spider.New()
+	for _, url := range urls {
+		docs, err := s.Scan(url, 2)
+		if err != nil {
+			return allDocs, err
+		}
+		allDocs = append(allDocs, docs...)
+	}
+	return allDocs, nil
+}
+
+func sendUrls(docs []crawler.Document, word string, conn net.Conn) error {
+	for _, doc := range docs {
+		if strings.Contains(doc.URL, word) || strings.Contains(doc.Title, word) {
+			msg := doc.URL + " " + doc.Title + "\n"
+			_, err := conn.Write([]byte(msg))
+			if err != nil {
+				return err
+			}
+		}
+	}
+	_, err := conn.Write([]byte("--the end--\n"))
+	return err
 }
