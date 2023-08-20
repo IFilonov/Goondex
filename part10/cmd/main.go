fdf591c80f3d9397c50b2725fae5642cf7c85619
Filonov <filonovia@gmail.com>
2023-08-20 21:28:47 +0500

    part15 game tennis ping pong

---
+++ part10/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	game "Goondex/part10/pkg/game"
+)
+
+func main() {
+	game.Run()
+}
+
+// .............
+// Player1: pong
+// Player2: ping
+// Player1: pong
+// Player2: ping
+// Player2: I win!
+// Player1: stop
+// Score: Player1 5 points; Player2 4 points;
+// Player1: begin
+// Player2: ping
+// Player2: I win!
+// Player1: stop
+// Score: Player1 5 points; Player2 5 points;
